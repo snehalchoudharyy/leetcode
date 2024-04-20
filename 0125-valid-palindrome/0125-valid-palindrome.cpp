e72ab8d3ce0696e6a66242aa4df1d59dfe915138
snehalchoudharyy <162476801+snehalchoudharyy@users.noreply.github.com>
2024-04-21 02:21:41 +0530

    Time: 4 ms (80.29%), Space: 8.6 MB (63.09%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,32 +1,17 @@
-#include<bits/stdc++.h>
-#include<iostream>
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string str,strlwr="";
-        
-        for(int i=0;i<s.size();i++){
-            if(s[i]>='A' && s[i]<='Z' || s[i]>='a' && s[i]<='z'|| s[i]>='0' && s[i]<='9'){
-                strlwr+=(char)tolower(s[i]);
-            }
-        }
-        
-            
-    string orignalstr=strlwr;
-    cout<<strlwr;
-        int r=(strlwr.size());
-        cout<<r;
-        for(int i=0;i<(r);i++){
-            
-            swap(strlwr[i],strlwr[r-1]);
-            r--;
-        }
-        if(orignalstr==strlwr){
-                return true;
-            }
-            else{
-                return false;
-            }
-            
-    }    
+       int start=0;
+       int end=s.size()-1;
+       while(start<=end){
+           if(!isalnum(s[start])){start++; continue;}
+           if(!isalnum(s[end])){end--;continue;}
+           if(tolower(s[start])!=tolower(s[end]))return false;
+           else{
+               start++;
+               end--;
+           }
+       }
+       return true;
+}
 };
