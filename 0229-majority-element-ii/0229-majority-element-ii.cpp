7da2baa50bf2037316baac2db09fd999078e9932
snehalchoudharyy <162476801+snehalchoudharyy@users.noreply.github.com>
2024-05-08 01:10:02 +0530

    Time: 3 ms (98.45%), Space: 18.3 MB (91.87%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -3,14 +3,40 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
         vector<int> ans;
         int n=nums.size();
-        map<int,int> mpp;
-        int min=(n/3)+1;
+        int cnt1=0,cnt2=0;
+        int el1=INT_MIN,el2=INT_MIN;
         for(int i=0;i<n;i++){
-            mpp[nums[i]]++;
-            if(mpp[nums[i]]==min){
-                ans.push_back(nums[i]);
+            if(cnt1==0 && nums[i]!=el2){
+                cnt1=1;
+                el1=nums[i];
+
+            }
+            else if(cnt2==0 && nums[i]!=el1){
+                cnt2=1;
+                el2=nums[i];
+
+            }
+            else if(nums[i]==el1) cnt1++;
+            else if(nums[i]==el2) cnt2++;
+            else{
+                cnt1--;
+                cnt2--;
             }
         }
+        cnt1=0,cnt2=0;
+        for(int i=0;i<n;i++){
+            if(el1==nums[i]) cnt1++;
+            if(el2==nums[i]) cnt2++;
+        }
+        int mini=(n/3)+1;
+        if(cnt1>=mini){
+            ans.push_back(el1);
+        }
+        if(cnt2>=mini){
+            ans.push_back(el2);
+        }
+        sort(ans.begin(),ans.end());
         return ans;
+
     }
 };
