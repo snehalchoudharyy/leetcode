4cb42473a717996f121a183f90651e6c56034187
snehalchoudharyy <162476801+snehalchoudharyy@users.noreply.github.com>
2025-09-14 23:06:37 +0530

    Time: 8 ms (43.86%), Space: 16.4 MB (6.08%) - LeetHub

--- LeetCode/Easy/0001-two-sum/0001-two-sum.cpp
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n=nums.size();
-        vector<int> ans;
-        map<int,int> mpp;
+        vector<int> res;
+        map<int,int> mp; //to store index corresponding to values
         for(int i=0;i<n;i++){
             int a=nums[i];
-            int more=target-a;
-            if(mpp.find(more)!=mpp.end()){
-                ans.push_back(mpp[more]);
-                ans.push_back(i);
+            int b=target-a;
+            if(mp.find(b)!=mp.end()){// if present in map push the index in result
+                res.push_back(i);
+                res.push_back(mp[b]);
             }
-            mpp[a]=i;
+            mp[a]=i;  //if not present push the number as key in map and mapped with index as its value
         }
-        return ans;
+        return res;
     }
 };
